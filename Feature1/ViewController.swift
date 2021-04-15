81d79fca436971eaee01f3f0c9182a8950dffe20
Apple <apple@Apples-MacBook-Pro.local>
2021-04-15 17:49:17 +0530

    ViewWillAppear Added

--- Feature1/ViewController.swift
+++ Feature1/ViewController.swift
@@ -38,6 +38,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
 
