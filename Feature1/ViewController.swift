b015b7ebe22057d62583641223a860f919d0e50f
Apple <apple@Apples-MacBook-Pro.local>
2021-04-15 18:02:18 +0530

    View Will Appear Removed

--- Feature1/ViewController.swift
+++ Feature1/ViewController.swift
@@ -40,10 +40,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-
 
 }
 
